d0ac5f52f93e2900c5308ec6f24fc8d9c53eef1a
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-16 14:11:48 -0300

    atualizado o readme, implementado post para criação de contas e validação se cpf ja existe

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (req, res) => {
+  const { name, cpf } = req.body;
+
+  const customerAlreadyExists = customers.some((customers) => (
+    customers.cpf === cpf
+  ))
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ message: 'Customer already exists.'})
+  }
+
+  customers.push({
+    name,
+    cpf,
+    id: uuidV4(),
+    statement: [],
+  })
+
+  return res.status(201).send('Criado com sucesso!');
+})
 
 app.listen(3333);
