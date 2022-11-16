5a5f82dcf11da20bdaa3df7e791589fa09688320
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-16 15:16:46 -0300

    criado rota para depósito com verificação de conta para saber se é possível ou não realizar

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) return res.status(400).json({ message: 'Customer not found.'});
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (req, res) => {
   const { name, cpf } = req.body;
 
@@ -28,14 +42,26 @@ app.post('/account', (req, res) => {
   return res.status(201).send('Criado com sucesso!');
 })
 
-app.get('/statement/:cpf', (req, res) => {
-  const { cpf } = req.params;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) return res.status(400).json({ message: 'Customer not found.'});
-
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
   return res.json(customer.statement);
+})
+
+app.post('/deposit', verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+
+  const { customer } = req;
+
+  const statementOperations = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperations);
+
+  return res.status(201).send('Deposit successfully added!');
 })
 
 app.listen(3333);
