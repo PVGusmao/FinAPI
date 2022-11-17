37a905234b992150ebddf7d452bef51bf1745a29
Paulo Victor <pvmg1@hotmail.com>
2022-11-16 23:26:35 -0300

    criados rotas para edição de nome de usuário, deleção de usuário e pegar dados da balance e account do usuário

--- src/index.js
+++ src/index.js
@@ -99,4 +99,47 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (req, res) => {
   return res.status(201).send('Saque feito com sucesso.')
 })
 
+app.get('/statement/date', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  const { date } = req.query;
+
+  const dateformat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateformat).toDateString());
+
+  return res.json(statement);
+})
+
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { name } = req.body;
+  const { customer } = req;
+
+  customer.name = name;
+
+  return res.status(201).send('Usuário atualizado com sucesso.')
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+
+  return res.json(customer);
+})
+
+app.delete('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  customers.splice(customer, 1);
+
+  return res.status(200).json(customers);
+})
+
+app.get('/balance', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  return res.json(balance);
+})
+
 app.listen(3333);
